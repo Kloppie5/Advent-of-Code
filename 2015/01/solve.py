bc44184d175a26ab38e778eb9f138524c0c7914a
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2022-01-09 15:20:06 +0100

    2015-01-2

--- 2015/01/solve.py
+++ 2015/01/solve.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 input_file = Path(__file__).parent / "input"
 
-def small_step (debug = False):
+def small_step_final_floor (debug = False):
   floor = 0
   with input_file.open() as f:
     data = f.read().strip()
@@ -14,7 +14,7 @@ def small_step (debug = False):
         print(c, floor)
   return floor
 
-def big_step (debug = False):
+def big_step_final_floor (debug = False):
   with input_file.open() as f:
     data = f.read().strip()
     floor = data.count("(") - data.count(")")
@@ -23,5 +23,23 @@ def big_step (debug = False):
       print(floor)
   return floor
 
+def small_step_first_time_basement (debug = False):
+  floor = 0
+  with input_file.open() as f:
+    data = f.read().strip()
+    for i in range(len(data)):
+      if data[i] == "(":
+        floor += 1
+      elif data[i] == ")":
+        floor -= 1
+      if debug:
+        print(i, floor)
+      if floor == -1:
+        return i + 1
+  return -1
+
 if __name__ == "__main__":
-  big_step(True)
+  r = big_step_final_floor()
+  print(f"Part 1: The final floor is {r}")
+  r = small_step_first_time_basement()
+  print(f"Part 2: The first time Santa enters the basement is at {r}")
