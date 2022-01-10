5981f9dd43df7704c9b653544b55d44a6620b4db
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2022-01-10 16:21:35 +0100

    2015-05-2

--- 2015/05/solve.py
+++ 2015/05/solve.py
@@ -30,12 +30,42 @@ def is_nice ( word, debug = False ) :
     print(f"{word} NICE")
   return True
 
+def is_nicer ( word, debug = False ):
+  pairs = {}
+  pair_index = -1
+  for i in range(0, len(word) - 1):
+    pair = word[i:i+2]
+    if pair not in pairs:
+      pairs[pair] = i
+    else:
+      if i - pairs[pair] != 1:
+        pair_index = i
+        break
+  if pair_index == -1:
+    if debug:
+      print(f"{word} NOT NICE, no pairs; {pairs}")
+    return False
+
+  for i in range(0, len(word) - 2):
+    if word[i] == word[i + 2]:
+      if debug:
+        print(f"{word} NICE, pair at {pair_index}")
+      return True
+  if debug:
+    print(f"{word} NOT NICE, no letters that repeat with one letter between them")
+  return False
+
+
 if __name__ == "__main__":
   nice = 0
+  nicer = 0
   with input_file.open() as f:
     data = f.read().strip()
     for line in data.split("\n"):
       if is_nice(line):
         nice += 1
+      if is_nicer(line):
+        nicer += 1
 
   print(f"Part 1: {nice} nice words")
+  print(f"Part 2: {nicer} nicer words")
