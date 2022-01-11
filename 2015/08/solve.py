a3ba5e3538d74c56995eb539afa699658c5d21aa
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2022-01-11 16:20:45 +0100

    2015-08-1

---
+++ 2015/08/solve.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+input_file = Path(__file__).parent / "input"
+
+def small_step_number_of_characters_in_string ( string ):
+  string = string[1:-1] # remove surrounding quotes
+  characters_in_string = 0
+
+  i = 0
+  while i < len(string): # python, why dont you have a proper for loop?
+    characters_in_string += 1
+    if string[i] != '\\':
+      i += 1
+      continue
+
+    # indexing i+1 is technically unsafe for improper input
+    if string[i+1] == 'x':
+      i += 4
+    elif string[i+1] == '\\':
+      i += 2
+    elif string[i+1] == '"':
+      i += 2
+    else:
+      print("Error: unknown escape sequence: " + string[i:])
+      exit(1)
+
+  return characters_in_string
+
+if __name__ == "__main__":
+  debug = False
+
+  total_characters_in_string = 0
+  total_characters_in_memory = 0
+  with open(input_file, "r") as f:
+    for line in f:
+      line = line.strip()
+      characters_in_string = small_step_number_of_characters_in_string(line)
+      characters_in_memory = len(line)
+      total_characters_in_string += characters_in_string
+      total_characters_in_memory += characters_in_memory
+      if debug :
+        print(f"{line} - {characters_in_string} characters in string, {characters_in_memory} characters in memory")
+  if debug :
+    print(f"Total characters in string: {total_characters_in_string}")
+    print(f"Total characters in memory: {total_characters_in_memory}")
+  print(f"Part 1: Difference: {total_characters_in_memory - total_characters_in_string}")
