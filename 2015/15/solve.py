df613e3f923f812bc9a69a077ac6dc2720bcbf52
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2022-01-19 17:12:04 +0100

    2015-15-2

--- 2015/15/solve.py
+++ 2015/15/solve.py
@@ -1,3 +1,5 @@
+import copy
+
 from pathlib import Path
 input_file = Path(__file__).parent / "input"
 
@@ -157,7 +159,90 @@ def decent_solve ( data, amount, debug = False ) :
 
   return recipe, stats, score
 
+def proper_solve ( data, teaspoons, calorie_target, debug = False ) :
+  if debug :
+    print(f"proper_solve(\n  data = {data},\n  teaspoons = {teaspoons},\n  calorie_target = {calorie_target}\n)")
+
+  # Scale everything based on the lowest calorie ingredient
+  lowest_calorie = min([data[ingredient]["calories"] for ingredient in data])
+  lowest_calorie_ingredient = None
+  for ingredient in data :
+    data[ingredient]["calories"] -= lowest_calorie
+    if data[ingredient]["calories"] == 0:
+      lowest_calorie_ingredient = ingredient
+
+  recipe = {}
+  stats = {}
+  for ingredient in data:
+    recipe[ingredient] = 0
+    for prop in data[ingredient]:
+      if prop not in stats:
+          stats[prop] = 0
+
+  recipe[lowest_calorie_ingredient] = teaspoons
+  for prop in data[lowest_calorie_ingredient]:
+    stats[prop] += data[lowest_calorie_ingredient][prop] * teaspoons
+  del stats["calories"]
+  minimum_calories = lowest_calorie * teaspoons
+
+  ingredients = list(data.keys())
+  ingredients.remove(lowest_calorie_ingredient)
+  props = list(stats.keys())
+
+  recipes = {}
+  recipes[minimum_calories] = {"ingredients": recipe, "stats": stats}
+
+  if debug :
+    print(f"Starting recipes: {recipes}")
+
+  for i in range(minimum_calories+1, calorie_target+1) :
+    if debug :
+      print(f"Trying to reach {i} calories")
+    best_recipe = None
+    least_worst_recipe = None
+    best_score = 0
+    least_worst_score = -100000
+    for ingredient in ingredients :
+      if i - data[ingredient]["calories"] in recipes :
+        i_recipe = copy.deepcopy(recipes[i - data[ingredient]["calories"]])
+        i_recipe["ingredients"][lowest_calorie_ingredient] -= 1
+        i_recipe["ingredients"][ingredient] += 1
+        for prop in props :
+          if prop in data[lowest_calorie_ingredient] :
+            i_recipe["stats"][prop] -= data[lowest_calorie_ingredient][prop]
+          if prop in data[ingredient] :
+            i_recipe["stats"][prop] += data[ingredient][prop]
+        if debug :
+          print(f"Found recipe: {i_recipe}")
+        i_score = i_recipe["stats"]["capacity"] * i_recipe["stats"]["durability"] * i_recipe["stats"]["flavor"] * i_recipe["stats"]["texture"]
+        least_worst_i_score = min([i_recipe["stats"][prop] for prop in props])
+        for prop in props :
+          if i_recipe["stats"][prop] < 0:
+            i_score = 0
+            break
+
+        if i_score > best_score :
+          best_recipe = i_recipe
+          best_score = i_score
+
+        if least_worst_i_score > least_worst_score :
+          least_worst_recipe = i_recipe
+          least_worst_score = least_worst_i_score
+
+    if best_recipe is not None :
+      if debug :
+        print(f"Found best recipe: {best_recipe}")
+      recipes[i] = best_recipe
+    elif least_worst_recipe is not None :
+      if debug :
+        print(f"Found least worst recipe: {least_worst_recipe}")
+      recipes[i] = least_worst_recipe
+
+  return recipes[calorie_target]["ingredients"], recipes[calorie_target]["stats"], recipes[calorie_target]["stats"]["capacity"] * recipes[calorie_target]["stats"]["durability"] * recipes[calorie_target]["stats"]["flavor"] * recipes[calorie_target]["stats"]["texture"]
+
 if __name__ == "__main__":
   data = parse_input(input_file)
-  recipe, stats, score = decent_solve(data, 100, True)
+  recipe, stats, score = decent_solve(data, 100)
   print(f"Part 1: {score}")
+  recipe, stats, score = proper_solve(data, 100, 500)
+  print(f"Part 2: {score}")
