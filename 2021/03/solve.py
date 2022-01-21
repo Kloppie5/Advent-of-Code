7b33a436f65d62872b194943bef4f5997783c7ff
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2022-01-21 20:55:29 +0100

    2021-03-2

--- 2021/03/solve.py
+++ 2021/03/solve.py
@@ -6,19 +6,42 @@ def parse_input ( input_file, debug = False ) :
     data = f.read().splitlines()
   return data
 
-def diagnostics ( data ) :
-  length = len(data)
-  # transpose and cast to int
-  data = [ map(int, x) for x in zip(*[ list(x) for x in data ]) ]
-  # most common bit
-  data = [ int(sum(x) > length / 2) for x in data ]
+def most_common_bit ( data, bit ) :
+  return int(bit_ratio(data, bit) > 0.5)
+def bit_ratio ( data, bit ) :
+  return sum([ int(x[bit]) for x in data ]) / len(data)
 
-  gamma_rate = int(''.join(map(str, data)), 2)
-  epsilon_rate = (1 << len(data)) - gamma_rate - 1
+def calculate_gamma_rate ( data ) :
+  return ''.join([ str(most_common_bit(data, i)) for i in range(len(data[0])) ])
 
-  return gamma_rate, epsilon_rate
+def calculate_epsilon_rate ( data ) :
+  return ''.join([ str(1-most_common_bit(data, i)) for i in range(len(data[0])) ])
+
+def calculate_oxygen_generator_rating ( data ) :
+  for i in range(len(data[0])):
+    ratio = bit_ratio(data, i)
+    criteria_bit = str(int(ratio >= 0.5))
+    data = [ x for x in data if x[i] == criteria_bit ]
+    if len(data) == 1 :
+      return data[0]
+
+def calculate_co2_scrubber_rating ( data ) :
+  for i in range(len(data[0])):
+    ratio = bit_ratio(data, i)
+    criteria_bit = str(int(ratio < 0.5))
+    data = [ x for x in data if x[i] == criteria_bit ]
+    if len(data) == 1 :
+      return data[0]
 
 if __name__ == "__main__":
   data = parse_input(input_file)
-  gamma_rate, epsilon_rate = diagnostics(data)
-  print(f"Part 1: {gamma_rate*epsilon_rate}")
+  gamma_rate = int(calculate_gamma_rate(data), 2)
+  epsilon_rate = int(calculate_epsilon_rate(data), 2)
+  oxygen_generator_rating = int(calculate_oxygen_generator_rating(data), 2)
+  co2_scrubber_rating = int(calculate_co2_scrubber_rating(data), 2)
+  print(f"Gamma rate: {gamma_rate}")
+  print(f"Epsilon rate: {epsilon_rate}")
+  print(f"Oxygen generator rating: {oxygen_generator_rating}")
+  print(f"CO2 scrubber rating: {co2_scrubber_rating}")
+  print(f"Part 1: {gamma_rate * epsilon_rate}")
+  print(f"Part 2: {oxygen_generator_rating * co2_scrubber_rating}")
