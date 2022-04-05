874f4951819c6c702c161796f821b84bdf9c655f
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2022-04-05 21:18:01 +0200

    2021-05-2

--- 2021/05/solve.py
+++ 2021/05/solve.py
@@ -34,6 +34,27 @@ def axial_overlap_board ( vents, debug = False ) :
 
   return board
 
+def overlap_board ( vents ) :
+  x_max = max([max(v["x1"], v["x2"]) for v in vents])
+  y_max = max([max(v["y1"], v["y2"]) for v in vents])
+  board = [[0 for x in range(x_max+1)] for y in range(y_max+1)]
+
+  for vent in vents :
+    dx = 1 if vent["x1"] <= vent["x2"] else -1
+    xs = range(vent["x1"], vent["x2"] + dx, dx)
+    dy = 1 if vent["y1"] <= vent["y2"] else -1
+    ys = range(vent["y1"], vent["y2"] + dy, dy)
+ 
+    if len(xs) == 1 :
+      xs = [xs[0]] * len(ys)
+    if len(ys) == 1 :
+      ys = [ys[0]] * len(xs)
+    
+    for x, y in zip(xs, ys) :
+      board[y][x] += 1
+  
+  return board
+
 def count_overlaps ( board ) :
   return sum([sum([1 for x in row if x > 1]) for row in board])
 
@@ -41,3 +62,5 @@ if __name__ == "__main__":
   vents = parse_input(input_file)
   board = axial_overlap_board(vents)
   print(f"Part 1: {count_overlaps(board)}")
+  board = overlap_board(vents)
+  print(f"Part 2: {count_overlaps(board)}")
