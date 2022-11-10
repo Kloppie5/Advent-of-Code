ecd805a5c71b62ed94be1769122f86fa9e40eef5
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2022-11-10 14:21:03 +0100

    2016-02-2

--- 2016/02/solve.py
+++ 2016/02/solve.py
@@ -28,4 +28,36 @@ for line in input :
   print(f"({x}, {y}) = {x + y * 3 + 1}")
   code += str(x + y * 3 + 1)
 
-print(f"Code: {code}")
+print(f"Simple code: {code}")
+
+# reset position
+x = 2
+y = 2
+keypad = [
+  [" ", " ", "1", " ", " "],
+  [" ", "2", "3", "4", " "],
+  ["5", "6", "7", "8", "9"],
+  [" ", "A", "B", "C", " "],
+  [" ", " ", "D", " ", " "]
+]
+complicated_code = ""
+
+# loop through instructions
+for line in input :
+  # loop through characters
+  for c in line :
+    # move
+    if c == "U" and y > 0 and keypad[y - 1][x] != " " :
+      y -= 1
+    elif c == "D" and y < 4 and keypad[y + 1][x] != " " :
+      y += 1
+    elif c == "L" and x > 0 and keypad[y][x - 1] != " " :
+      x -= 1
+    elif c == "R" and x < 4 and keypad[y][x + 1] != " " :
+      x += 1
+  
+  # print button
+  print(f"({x}, {y}) = {keypad[y][x]}")
+  complicated_code += keypad[y][x]
+
+print(f"Complicated code: {complicated_code}")
