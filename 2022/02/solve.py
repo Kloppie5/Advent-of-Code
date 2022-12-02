ffe53e9d2ea8f9f0be41a8b437281a86bfaa7015
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2022-12-02 17:48:59 +0100

    2022-02-2

--- 2022/02/solve.py
+++ 2022/02/solve.py
@@ -6,7 +6,7 @@ def parse_input ( debug = False ) :
         data = f.read().splitlines()
     return data
 
-score_lut = {
+score_lut_1 = {
     'A X' : 1 + 3, # 1 point for X, 3 points for draw
     'B X' : 1 + 0, # 1 point for X, 0 points for loss
     'C X' : 1 + 6, # 1 point for X, 6 points for win
@@ -17,11 +17,30 @@ score_lut = {
     'B Z' : 3 + 6, # 3 points for Z, 6 points for win
     'C Z' : 3 + 3, # 3 points for Z, 3 points for draw
 }
-def score_game ( game ) :
-    return score_lut[game]
+def score_game_1 ( game ) :
+    return score_lut_1[game]
+
+score_lut_2 = {
+    'A X' : 3 + 0, # 3 points for C, 0 points for loss
+    'B X' : 1 + 0, # 1 point for A, 0 points for loss
+    'C X' : 2 + 0, # 2 points for B, 0 points for loss
+    'A Y' : 1 + 3, # 1 point for A, 3 points for draw
+    'B Y' : 2 + 3, # 2 points for B, 3 points for draw
+    'C Y' : 3 + 3, # 3 points for C, 3 points for draw
+    'A Z' : 2 + 6, # 2 points for B, 6 points for win
+    'B Z' : 3 + 6, # 3 points for C, 6 points for win
+    'C Z' : 1 + 6, # 1 point for A, 6 points for win
+}
+def score_game_2 ( game ) :
+    return score_lut_2[game]
 
 if __name__ == "__main__":
     games = parse_input()
-    scores = [ score_game(game) for game in games ]
-    total_score = sum(scores)
-    print(f"Part 1: {total_score}")
+    
+    scores_1 = [ score_game_1(game) for game in games ]
+    total_score_1 = sum(scores_1)
+    print(f"Part 1: {total_score_1}")
+
+    scores_2 = [ score_game_2(game) for game in games ]
+    total_score_2 = sum(scores_2)
+    print(f"Part 2: {total_score_2}")
