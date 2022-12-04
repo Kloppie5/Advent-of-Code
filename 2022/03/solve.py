d77020be9fa7f28840919bcf027fbbb4e3e229c6
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2022-12-04 21:40:18 +0100

    2022-03-2

--- 2022/03/solve.py
+++ 2022/03/solve.py
@@ -24,3 +24,19 @@ if __name__ == "__main__":
         priority = ord(matching_letter) - 38 if matching_letter.isupper() else ord(matching_letter) - 96
         total_priority += priority
     print(f"Part 1: total priority is {total_priority}")
+
+    total_priority = 0
+    for i in range(0, len(rucksacks), 3):
+        elf_1, elf_2, elf_3 = rucksacks[i], rucksacks[i+1], rucksacks[i+2]
+        
+        matching_letter = None
+        for letter in elf_1:
+            if letter in elf_2 and letter in elf_3:
+                matching_letter = letter
+                break
+        else:
+            print("No matching letter found")
+    
+        priority = ord(matching_letter) - 38 if matching_letter.isupper() else ord(matching_letter) - 96
+        total_priority += priority
+    print(f"Part 2: total priority is {total_priority}")
