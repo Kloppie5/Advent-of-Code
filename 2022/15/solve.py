526dca610dbb00e23f72731a4d307da0025be1b8
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2022-12-16 00:01:01 +0100

    2022-15-2

--- 2022/15/solve.py
+++ 2022/15/solve.py
@@ -57,3 +57,46 @@ if __name__ == "__main__" :
     print(data)
     positions = sum([interval[1] - interval[0] + 1 for interval in data])
     print(f"Part 1: {positions} {beacons_on_the_line} => {positions - len(beacons_on_the_line)}")
+
+    data = parse_input("input")
+    sum_lines = []
+    diff_lines = []
+    for i in range(len(data)) :
+        distance = abs(data[i]["beacon"][0] - data[i]["sensor"][0]) + abs(data[i]["beacon"][1] - data[i]["sensor"][1])
+        sum_lines += [data[i]["sensor"][0] + data[i]["sensor"][1] - distance - 1]
+        sum_lines += [data[i]["sensor"][0] + data[i]["sensor"][1] + distance + 1]
+        diff_lines += [data[i]["sensor"][0] - data[i]["sensor"][1] - distance - 1]
+        diff_lines += [data[i]["sensor"][0] - data[i]["sensor"][1] + distance + 1]
+    sum_lines.sort()
+    diff_lines.sort()
+
+    sum_lines = [sum_lines[i] for i in range(len(sum_lines)-1) if sum_lines[i] == sum_lines[i+1]]
+    diff_lines = [diff_lines[i] for i in range(len(diff_lines)-1) if diff_lines[i] == diff_lines[i+1]]
+
+    possibilities = []
+    for sum_line in sum_lines :
+        for diff_line in diff_lines :
+            if (sum_line + diff_line) % 2 == 0 :
+                x = (sum_line + diff_line) // 2
+                y = (sum_line - diff_line) // 2
+                possibilities += [(x, y)]
+    print(f"Possibilities: {possibilities}")
+
+    solution_x = 0
+    solution_y = 0
+    for x, y in possibilities :
+        print(f"Checking ({x}, {y})")
+        for entry in data :
+            distance = abs(entry["beacon"][0] - entry["sensor"][0]) + abs(entry["beacon"][1] - entry["sensor"][1])
+            if abs(entry["sensor"][0] - x) + abs(entry["sensor"][1] - y) < distance :
+                print(f" X ({x}, {y}) falls within range of {entry}")
+                break
+        else :
+            print(f" ! ({x}, {y}) is a valid position")
+            solution_x = x
+            solution_y = y
+    
+    print(f"Part 2: {solution_x} {solution_y} => {solution_x * 4000000 + solution_y}")
+    
+
+        
