9bcdbe3623a926f0c3b24158a8a2dcef9c85c1bb
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2022-12-15 18:53:35 +0100

    2022-13-2

--- 2022/13/solve.py
+++ 2022/13/solve.py
@@ -1,12 +1,22 @@
 import sys
 sys.stdout.reconfigure(encoding='utf-8')
 
-def parse_input ( filename ) :
+def parse_input ( filename, mode ) :
     from pathlib import Path
     with open(Path(__file__).parent / filename, "r") as f :
-        data = f.read().split("\n\n")
-    data = [line.splitlines() for line in data]
-    data = [(eval(left), eval(right)) for left, right in data]
+        data = f.read()
+
+    if mode == "pairs" :
+        data = data.split("\n\n")
+        data = [line.splitlines() for line in data]
+        data = [(eval(left), eval(right)) for left, right in data]
+    elif mode == "singles" :
+        data = data.splitlines()
+        data = [line for line in data if line]
+        data = [eval(line) for line in data]
+    else :
+        raise Exception("Unknown mode")
+    
     return data
 
 def compare_pair ( left, right ) :
@@ -35,7 +45,7 @@ def compare_pair ( left, right ) :
     return -2
 
 if __name__ == "__main__" :
-    pairs = parse_input("input")
+    pairs = parse_input("input", "pairs")
     
     right_order = []
     for i, pair in enumerate(pairs):
@@ -45,3 +55,20 @@ if __name__ == "__main__" :
             right_order.append(i+1)
     print(f"Right order: {right_order}")
     print(f"Part 1: {sum(right_order)}")
+
+    packets = parse_input("input", "singles")
+    pre_2 = 0
+    pre_6 = 0
+    for i, packet in enumerate(packets):
+        result = compare_pair(packet, [[2]])
+        if result == 1:
+            pre_2 += 1
+        result = compare_pair(packet, [[6]])
+        if result == 1:
+            pre_6 += 1
+
+    index_2 = pre_2 + 1
+    index_6 = pre_6 + 2
+    print(f"Index 2: {index_2}")
+    print(f"Index 6: {index_6}")
+    print(f"Part 2: {index_2 * index_6}")
--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 | Day 10 | :star: :star: |               |               |               |               |               |               | :star: :star: |
 | Day 11 | :star: :star: |               |               |               |               |               |               | :star: :star: |
 | Day 12 | :star: :star: |               |               |               |               |               |               | :star: :star: |
-| Day 13 | :star: :star: |               |               |               |               |               |               | :star:        |
+| Day 13 | :star: :star: |               |               |               |               |               |               | :star: :star: |
 | Day 14 | :star: :star: |               |               |               |               |               |               |               |
 | Day 15 | :star: :star: |               |               |               |               |               |               |               |
 | Day 16 | :star: :star: |               |               |               |               |               |               |               |
