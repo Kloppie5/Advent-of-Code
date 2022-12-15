a0f38f6d4bd6ab0bfbfddc9bbea14e3dec35945d
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2022-12-15 21:28:03 +0100

    2022-14-2

--- 2022/14/solve.py
+++ 2022/14/solve.py
@@ -7,7 +7,7 @@ def parse_input ( filename ) :
         data = f.read().splitlines()
     return data
 
-def make_world ( paths ) :
+def make_world ( paths, floor = False ) :
     walls = []
     for path in paths :
         points = path.split(" -> ")
@@ -24,10 +24,12 @@ def make_world ( paths ) :
                     walls += [(x, y)]
 
     width = max([wall[0] for wall in walls]) + 1
-    height = max([wall[1] for wall in walls]) + 1
+    height = max([wall[1] for wall in walls]) + 1 + 2
     world = [["." for x in range(width)] for y in range(height)]
     for wall in walls :
         world[wall[1]][wall[0]] = "#"
+    if floor :
+        world[height - 1] = "X" * width
     return world
 
 def print_world ( world ) :
@@ -36,7 +38,14 @@ def print_world ( world ) :
 
 def add_sand ( world, pos ) :
     x, y = pos
+    if world[y][x] == "o" :
+        return False
     while y < len(world) - 1 :
+        if x == len(world[y]) - 1 :
+            print(f"Expanding world to the right")
+            for i in range(y, len(world)-1) :
+                world[i] += "."
+            world[-1] += world[-1][-1]
         if world[y + 1][x] == "." :
             y += 1
         elif x > 0 and world[y + 1][x - 1] == "." :
@@ -47,22 +56,24 @@ def add_sand ( world, pos ) :
             y += 1
         else :
             world[y][x] = "o"
-            print(f"Stopped at {x}, {y}")
             return True
-    print(f"Reached bottom at {x}, {y}")
     return False
+
+def fill_world ( world, source ) :
+    while add_sand(world, source) :
+        pass
 
 if __name__ == "__main__" :
     paths = parse_input("input")
+
     world = make_world(paths)
     world[0][500] = "+"
-
-    i = 0
-    while True :
-        i += 1
-        print(f"Step {i}")
-        r = add_sand(world, (500, 0))
-        if not r :
-            print_world(world)
-            print(f"Part 1: {sum([line.count('o') for line in world])}")
-            break
+    fill_world(world, (500, 0))
+    print_world(world)
+    print(f"Part 1: {sum([line.count('o') for line in world])}")
+    
+    world = make_world(paths, floor = True)
+    world[0][500] = "+"
+    fill_world(world, (500, 0))
+    print_world(world)
+    print(f"Part 2: {sum([line.count('o') for line in world])}")
