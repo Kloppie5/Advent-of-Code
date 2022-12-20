9a332af609307deaf783a59971ce679bfdd3d7d1
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2022-12-20 16:29:22 +0100

    2022-20-2

--- 2022/20/solve.py
+++ 2022/20/solve.py
@@ -12,18 +12,25 @@ def parse_input ( filename ) :
 # Decryption therefore moves what number would reach the target position by moving
 #  backwards by their value
 # I'm an idiot; I just need to mix it once
-def mix ( data ) :
+def mix ( data, rounds ) :
     mutation_table = [i for i in range(len(data))]
-    for i, value in enumerate(data) :
-        index = mutation_table.index(i)
-        mutation_table.pop(index)
-        mutation_table.insert((index + value) % len(mutation_table), i)
+    for r in range(rounds) :
+        for i, value in enumerate(data) :
+            index = mutation_table.index(i)
+            mutation_table.pop(index)
+            mutation_table.insert((index + value) % len(mutation_table), i)
     return [data[x] for x in mutation_table]
 
 if __name__ == "__main__" :
-    data = parse_input("input")
-
-    mixed = mix(data)
-
+    raw_data = parse_input("input")
+    
+    data = raw_data.copy()
+    mixed = mix(data, 1)
     zero_index = mixed.index(0)
-    print(f"Part 1: {mixed[(zero_index + 1000)%len(mixed)]}, {mixed[(zero_index + 2000)%len(mixed)]}, {mixed[(zero_index + 3000)%len(mixed)]} = {mixed[(zero_index + 1000)%len(mixed)] + mixed[(zero_index + 2000)%len(mixed)] + mixed[(zero_index + 3000)%len(mixed)]}")
+    print(f"Part 1: ({mixed[(zero_index + 1000)%len(mixed)]}, {mixed[(zero_index + 2000)%len(mixed)]}, {mixed[(zero_index + 3000)%len(mixed)]}) = {mixed[(zero_index + 1000)%len(mixed)] + mixed[(zero_index + 2000)%len(mixed)] + mixed[(zero_index + 3000)%len(mixed)]}")
+
+    data = raw_data.copy()
+    data = [data[i] * 811589153 for i in range(len(data))]
+    mixed = mix(data, 10)
+    zero_index = mixed.index(0)
+    print(f"Part 2: ({mixed[(zero_index + 1000)%len(mixed)]}, {mixed[(zero_index + 2000)%len(mixed)]}, {mixed[(zero_index + 3000)%len(mixed)]}) = {mixed[(zero_index + 1000)%len(mixed)] + mixed[(zero_index + 2000)%len(mixed)] + mixed[(zero_index + 3000)%len(mixed)]}")
